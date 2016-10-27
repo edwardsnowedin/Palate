6166d5dd9c1e7c3a011ea3b6757877310310b07f
Matthew Paz <paz.matthew@me.com>
2016-10-27 14:30:44 -0400

    Form is born!

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const logger  = require('logger');
+const logger  = require('morgan');
 
 const app  = express();
 const port = process.env.PORT || process.argv[2] || 3000;
 
-app.set('view engine', 'views');
+app.set('view engine', 'ejs');
 
 app.listen(port, () => console.log('Server is listening on', `${port}`));
 
