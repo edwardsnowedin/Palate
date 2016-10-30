32e988123ad70914557145d609e8c64ea07f2649
Matthew Paz <paz.matthew@me.com>
2016-10-30 19:52:18 -0400

    Map is alive

--- app.js
+++ app.js
@@ -1,31 +1,58 @@
 const dotEnv          = require('dotenv').config({silent: true});
 const express         = require('express');
-const logger         = require('morgan');
+const morgan          = require('morgan');
 const path            = require('path');
 const bodyParser      = require('body-parser');
 const session         = require('express-session');
 const cookieParser    = require('cookie-parser');
 const methodOverride  = require('method-override');
+// const indexRouter     = require('./routes/index.js');
 // const authRouter      = require('./routes/auth');
 // const usersRouter     = require('./routes/users');
-const homeRoute       = require('./routes/home');
-const mapRoute        = require('./routes/map');
-const loginRoute      = require('./routes/login');
-const signupRoute     = require('./routes/signUp');
+const homeRoute       = require('./routes/home.js');
+const loginRoute      = require('./routes/login.js');
+const mapRoute        = require('./routes/map.js');
+const signupRoute     = require('./routes/signUp.js');
 
-const app  = express();
-const port = process.env.PORT || process.argv[2] || 3000;
+
+
+const app             = express();
+const SECRET          = 'tacos3000';
+
 
 app.set('view engine', 'ejs');
-app.use(logger('dev'));
 
-app.use(express.static(__dirname + '/public'));
+// log requests to STDOUT
+app.use(morgan('dev'));
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => console.log('Server is listening on', `${port}`));
+// parse application/json
+app.use(bodyParser.json());
 
+// middleware for method override
+app.use(methodOverride('_method'));
+
+// This is how we read the cookies sent over from the browser
+app.use(cookieParser());
+
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: SECRET
+}));
+
+// Set static file root folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homeRoute);
+app.use('/userLogin', loginRoute);
 app.use('/map', mapRoute);
-app.use('/login', loginRoute);
 app.use('/signUp', signupRoute);
+
+
+// Listen on port for connections
+// process.env.PORT is needed for when we deploy to Heroku
+const port = process.env.PORT || process.argv[2] || 3000;
+app.listen(port, () => console.log('Server is listening on', `${port}`));
--- public/script/map.js
+++ public/script/map.js
@@ -1,10 +1,44 @@
 console.log('js is linked!');
 
-let map;
-function initMap() {
-  // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
-    zoom: 13
-});
+// let map;
+// function initMap() {
+//   // Constructor creates a new map - only center and zoom are required.
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 40.7413549, lng: -73.9980244},
+//     zoom: 13
+// });
+//
+// }
+function success(position) {
+  var mapcanvas = document.createElement('div');
+  mapcanvas.id = 'mapcontainer';
+  mapcanvas.style.height = '400px';
+  mapcanvas.style.width = '600px';
+
+  document.querySelector('article').appendChild(mapcanvas);
+
+  var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+  var options = {
+    zoom: 13,
+    center: coords,
+    mapTypeControl: false,
+    navigationControlOptions: {
+    	style: google.maps.NavigationControlStyle.SMALL
+    },
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
+
+  var marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+      title:"You are here!"
+  });
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success);
+} else {
+  error('Geo Location is not supported');
 }
